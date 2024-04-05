c78d36711cacb219b46b0a34a2ce3371be0475c0
Agnes Kyuun Park <agnespark0413@gmail.com>
2024-04-05 11:56:59 +0900

    [feat] custom datetimepicker UI 다듬는 중

--- lib/pages/new_datetime/newdatetime.dart
+++ lib/pages/new_datetime/newdatetime.dart
@@ -1,6 +1,5 @@
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 import 'package:flutter/material.dart';
-import 'package:namer_app/pages/new_datetime/timepicker.dart';
 
 final today = DateUtils.dateOnly(DateTime.now());
 
@@ -51,60 +50,62 @@ class _NewDateTimePageState extends State<NewDateTimePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black), // 사방으로 보더 적용
-                    borderRadius:
-                        BorderRadius.all(Radius.circular(12)), // 보더의 모서리를 둥글게 함
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      _getValueText(
-                        CalendarDatePicker2Type.single,
-                        _singleDatePickerValueWithDefaultValue,
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black), // 사방으로 보더 적용
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(12)), // 보더의 모서리를 둥글게 함
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _getValueText(
+                          CalendarDatePicker2Type.single,
+                          _singleDatePickerValueWithDefaultValue,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black), // 테두리 선 설정
-              ),
-              width: 480,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: 300, // 첫 번째 자식 위젯의 폭을 200으로 설정
-                    child:
-                        _buildDefaultSingleDatePickerWithValue(), // 예시로 사용된 함수
-                  ),
-                  // SizedBox(height: 16), // 위젯 사이 간격 조정을 위한 SizedBox
-                  Container(
-                    width: 1, // Divider의 너비를 1로 설정
-                    height: 300,
-                    color: Colors.black, // Divider의 색상 설정
-                  ),
-                  Container(
-                    width: 150,
-                    child: _setTimeNumberValue(),
-                  ),
                 ],
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black), // 테두리 선 설정
+                ),
+                width: 453,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 300, // 첫 번째 자식 위젯의 폭을 200으로 설정
+                      child:
+                          _buildDefaultSingleDatePickerWithValue(), // 예시로 사용된 함수
+                    ),
+                    // SizedBox(height: 16), // 위젯 사이 간격 조정을 위한 SizedBox
+                    Container(
+                      width: 1, // Divider의 너비를 1로 설정
+                      height: 350,
+                      color: Colors.black, // Divider의 색상 설정
+                    ),
+                    Container(
+                      width: 150,
+                      child: _setTimeNumberValue(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -119,6 +120,10 @@ class _NewDateTimePageState extends State<NewDateTimePage> {
     var valueText = (values.isNotEmpty ? values[0] : null)
         .toString()
         .replaceAll('00:00:00.000', '');
+
+    // Add selected time to the displayed text
+    valueText += ' ${selectedHour.toString().padLeft(2, '0')}:'
+        '${selectedMinute.toString().padLeft(2, '0')}';
 
     if (datePickerType == CalendarDatePicker2Type.multi) {
       valueText = values.isNotEmpty
@@ -139,161 +144,6 @@ class _NewDateTimePageState extends State<NewDateTimePage> {
     }
 
     return valueText;
-  }
-
-  _buildCalendarDialogButton() {
-    const dayTextStyle =
-        TextStyle(color: Colors.black, fontWeight: FontWeight.w700);
-    final weekendTextStyle =
-        TextStyle(color: Colors.grey[500], fontWeight: FontWeight.w600);
-    final anniversaryTextStyle = TextStyle(
-      color: Colors.red[400],
-      fontWeight: FontWeight.w700,
-      decoration: TextDecoration.underline,
-    );
-    // config 변수
-    final config = CalendarDatePicker2WithActionButtonsConfig(
-      calendarViewScrollPhysics: const NeverScrollableScrollPhysics(),
-      dayTextStyle: dayTextStyle,
-      calendarType: CalendarDatePicker2Type.range,
-      selectedDayHighlightColor: Colors.purple[800],
-      closeDialogOnCancelTapped: true,
-      firstDayOfWeek: 1,
-      weekdayLabelTextStyle: const TextStyle(
-        color: Colors.black87,
-        fontWeight: FontWeight.bold,
-      ),
-      controlsTextStyle: const TextStyle(
-        color: Colors.black,
-        fontSize: 15,
-        fontWeight: FontWeight.bold,
-      ),
-      centerAlignModePicker: true,
-      customModePickerIcon: const SizedBox(),
-      selectedDayTextStyle: dayTextStyle.copyWith(color: Colors.white),
-      dayTextStylePredicate: ({required date}) {
-        TextStyle? textStyle;
-        if (date.weekday == DateTime.saturday ||
-            date.weekday == DateTime.sunday) {
-          textStyle = weekendTextStyle;
-        }
-        if (DateUtils.isSameDay(date, DateTime(2021, 1, 25))) {
-          textStyle = anniversaryTextStyle;
-        }
-        return textStyle;
-      },
-      dayBuilder: ({
-        required date,
-        textStyle,
-        decoration,
-        isSelected,
-        isDisabled,
-        isToday,
-      }) {
-        Widget? dayWidget;
-        if (date.day % 3 == 0 && date.day % 9 != 0) {
-          dayWidget = Container(
-            decoration: decoration,
-            child: Center(
-              child: Stack(
-                alignment: AlignmentDirectional.center,
-                children: [
-                  Text(
-                    MaterialLocalizations.of(context).formatDecimal(date.day),
-                    style: textStyle,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 27.5),
-                    child: Container(
-                      height: 4,
-                      width: 4,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: isSelected == true
-                            ? Colors.white
-                            : Colors.grey[500],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        }
-        return dayWidget;
-      },
-      yearBuilder: ({
-        required year,
-        decoration,
-        isCurrentYear,
-        isDisabled,
-        isSelected,
-        textStyle,
-      }) {
-        return Center(
-          child: Container(
-            decoration: decoration,
-            height: 36,
-            width: 72,
-            child: Center(
-              child: Semantics(
-                selected: isSelected,
-                button: true,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      year.toString(),
-                      style: textStyle,
-                    ),
-                    if (isCurrentYear == true)
-                      Container(
-                        padding: const EdgeInsets.all(5),
-                        margin: const EdgeInsets.only(left: 5),
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.redAccent,
-                        ),
-                      ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        );
-      },
-    );
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              final values = await showCalendarDatePicker2Dialog(
-                context: context,
-                config: config,
-                dialogSize: const Size(325, 400),
-                borderRadius: BorderRadius.circular(15),
-                value: _dialogCalendarPickerValue,
-                dialogBackgroundColor: Colors.white,
-              );
-              if (values != null) {
-                // ignore: avoid_print
-                print(_getValueText(
-                  config.calendarType,
-                  values,
-                ));
-                setState(() {
-                  _dialogCalendarPickerValue = values;
-                });
-              }
-            },
-            child: const Text('Open Calendar Dialog'),
-          ),
-        ],
-      ),
-    );
   }
 
   Widget _buildDefaultSingleDatePickerWithValue() {
@@ -339,77 +189,116 @@ class _NewDateTimePageState extends State<NewDateTimePage> {
     return Row(
       children: [
         Expanded(
-          child: SizedBox(
-            height: 300,
-            child: ListView(
-              children: List.generate(
-                24,
-                (index) {
-                  final hour = index;
-                  return ListTile(
-                    title: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: selectedHour == hour ? Colors.blue : null,
-                      ),
-                      // padding: EdgeInsets.all(8),
-                      child: Center(
-                        child: Text(
-                          '$hour',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: selectedHour == hour ? Colors.white : null,
-                          ),
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        selectedHour = hour;
-                      });
-                    },
-                  );
-                },
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8), // 위아래 패딩 설정
+                child: Text(
+                  'HOUR',
+                ),
               ),
-            ),
+              Container(
+                height: 1, // Divider의 높이
+                width: double.infinity, // Divider의 가로폭을 가득 채우도록 설정
+                color: Colors.black, // Divider의 색상
+              ),
+              Container(
+                height: 310,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: List.generate(
+                      24,
+                      (index) {
+                        final hour = index;
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedHour = hour;
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: selectedHour == hour ? Colors.blue : null,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '$hour',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: selectedHour == hour
+                                      ? Colors.white
+                                      : null,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
-        Divider(color: Colors.black),
         Expanded(
-          child: SizedBox(
-            height: 300,
-            child: ListView(
-              children: List.generate(
-                60,
-                (index) {
-                  final minute = index;
-                  return ListTile(
-                    title: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: selectedMinute == minute ? Colors.blue : null,
-                      ),
-                      // padding: EdgeInsets.all(8),
-                      child: Center(
-                        child: Text(
-                          '$minute',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color:
-                                selectedMinute == minute ? Colors.white : null,
-                          ),
-                        ),
-                      ),
-                    ),
-                    onTap: () {
-                      setState(() {
-                        selectedMinute = minute;
-                      });
-                    },
-                  );
-                },
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 8), // 위아래 패딩 설정
+                child: Text(
+                  'MINUTE',
+                ),
               ),
-            ),
+              Container(
+                height: 1, // Divider의 높이
+                width: double.infinity, // Divider의 가로폭을 가득 채우도록 설정
+                color: Colors.black, // Divider의 색상
+              ),
+              Container(
+                height: 310,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    children: List.generate(
+                      60,
+                      (index) {
+                        final minute = index;
+                        return GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              selectedMinute = minute;
+                            });
+                          },
+                          child: Container(
+                            height: 30,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color:
+                                  selectedMinute == minute ? Colors.blue : null,
+                            ),
+                            child: Center(
+                              child: Text(
+                                '$minute',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: selectedMinute == minute
+                                      ? Colors.white
+                                      : null,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ],
