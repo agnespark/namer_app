ad403d466bf6345513227a20ff65eb8976f4c920
Agnes Kyuun Park <agnespark0413@gmail.com>
2024-04-17 10:34:01 +0900

    [refactor] datetimepicker controller 정리

--- lib/pages/new_datetime/newdatetime_controller.dart
+++ lib/pages/new_datetime/newdatetime_controller.dart
@@ -5,22 +5,18 @@ class NewDateTimeController extends GetxController {
   RxInt selectedStartMinute = 0.obs;
   RxInt tempSelectedStartHour = 0.obs;
   RxInt tempSelectedStartMinute = 0.obs;
-  Rx<DateTime?> selectedStartDateTime = DateTime.now().obs;
-
-  RxBool isStart = true.obs;
+  Rx<DateTime?> selectedStartDateTime = Rx<DateTime?>(null);
 
   RxInt selectedEndHour = 0.obs;
   RxInt selectedEndMinute = 0.obs;
   RxInt tempSelectedEndHour = 0.obs;
   RxInt tempSelectedEndMinute = 0.obs;
-  Rx<DateTime?> selectedEndDateTime = DateTime.now().obs;
+  Rx<DateTime?> selectedEndDateTime = Rx<DateTime?>(null);
 
-  RxList<DateTime?> startDefaultValue = [
-    DateTime.now(),
-  ].obs;
-  RxList<DateTime?> endDefaultValue = [
-    DateTime.now().add(Duration(days: 7)),
-  ].obs;
+  RxList<DateTime?> startDefaultValue = RxList();
+  RxList<DateTime?> endDefaultValue = RxList();
+
+  RxBool isStart = true.obs;
 
   RxString acs = "오름차순".obs;
   RxString decs = "내림차순".obs;
@@ -29,14 +25,20 @@ class NewDateTimeController extends GetxController {
   void onInit() {
     super.onInit();
     DateTime now = DateTime.now();
+
     selectedStartHour.value = now.hour;
     selectedStartMinute.value = now.minute;
     tempSelectedStartHour.value = now.hour;
     tempSelectedStartMinute.value = now.minute;
+    selectedStartDateTime.value = now;
 
     selectedEndHour.value = now.hour;
     selectedEndMinute.value = now.minute;
     tempSelectedEndHour.value = now.hour;
     tempSelectedEndMinute.value = now.minute;
+    selectedEndDateTime.value = now;
+
+    startDefaultValue.add(now);
+    endDefaultValue.add(now.add(Duration(days: 7)));
   }
 }
