c4d4a898e5fe40eaaf1cfc9d47f1d1aaef75677a
Agnes Kyuun Park <agnespark0413@gmail.com>
2024-04-18 17:00:07 +0900

    [refactor] loadable_table 리팩토링

--- lib/component/table/loadable-table/loadable_table.dart
+++ lib/component/table/loadable-table/loadable_table.dart
@@ -1,53 +1,50 @@
-import 'dart:math';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:namer_app/component/table/loadable-table/loadable_table_controller.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class LoadableTable extends StatefulWidget {
   LoadableTable({Key? key}) : super(key: key);
 
   @override
-  _LoadMoreInfiniteScrollingDemoState createState() =>
-      _LoadMoreInfiniteScrollingDemoState();
+  _LoadableTableState createState() => _LoadableTableState();
 }
 
-class _LoadMoreInfiniteScrollingDemoState extends State<LoadableTable> {
-  List<Employee> _employees = <Employee>[];
-  late EmployeeDataSource _employeeDataSource;
-
-  @override
-  void initState() {
-    _populateEmployeeData(20);
-    _employeeDataSource = EmployeeDataSource(employees: _employees);
-    super.initState();
-  }
+class _LoadableTableState extends State<LoadableTable> {
+  final LoadableTableController controller = Get.put(LoadableTableController());
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: SfDataGrid(
-        source: _employeeDataSource,
+        source: controller.employeeDataSource,
         loadMoreViewBuilder: (BuildContext context, LoadMoreRows loadMoreRows) {
           Future<String> loadRows() async {
             await loadMoreRows();
-            return Future<String>.value('Completed');
+            return 'Completed';
           }
 
           return FutureBuilder<String>(
             initialData: 'loading',
             future: loadRows(),
-            builder: (context, snapShot) {
-              if (snapShot.data == 'loading') {
+            builder: (context, snapshot) {
+              if (snapshot.data == 'loading') {
                 return Container(
-                    height: 60.0,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        border: BorderDirectional(
-                            top: BorderSide(
-                                width: 1.0,
-                                color: Color.fromRGBO(0, 0, 0, 0.26)))),
-                    alignment: Alignment.center,
-                    child: CircularProgressIndicator());
+                  height: 60.0,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: BorderDirectional(
+                      top: BorderSide(
+                        width: 1.0,
+                        color: Color.fromRGBO(0, 0, 0, 0.26),
+                      ),
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: CircularProgressIndicator(),
+                );
               } else {
                 return SizedBox.fromSize(size: Size.zero);
               }
@@ -56,147 +53,91 @@ class _LoadMoreInfiniteScrollingDemoState extends State<LoadableTable> {
         },
         columns: <GridColumn>[
           GridColumn(
-              columnName: 'id',
-              label: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'ID',
-                  ))),
+            columnName: 'id',
+            label: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.center,
+              child: Text('ID'),
+            ),
+          ),
           GridColumn(
-              columnName: 'name',
-              label: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  alignment: Alignment.center,
-                  child: Text('Name'))),
+            columnName: 'name',
+            label: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.center,
+              child: Text('Name'),
+            ),
+          ),
           GridColumn(
-              width: 120.0,
-              columnName: 'designation',
-              label: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Designation',
-                    overflow: TextOverflow.ellipsis,
-                  ))),
+            width: 120.0,
+            columnName: 'designation',
+            label: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.center,
+              child: Text(
+                'Designation',
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
           GridColumn(
-              columnName: 'salary',
-              label: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  alignment: Alignment.center,
-                  child: Text('Salary'))),
+            columnName: 'salary',
+            label: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              alignment: Alignment.center,
+              child: Text('Salary'),
+            ),
+          ),
         ],
       ),
     );
   }
-
-  void _populateEmployeeData(int count) {
-    final Random _random = Random();
-    int startIndex = _employees.isNotEmpty ? _employees.length : 0,
-        endIndex = startIndex + count;
-    for (int i = startIndex; i < endIndex; i++) {
-      _employees.add(Employee(
-        1000 + i,
-        _names[_random.nextInt(_names.length - 1)],
-        _designation[_random.nextInt(_designation.length - 1)],
-        10000 + _random.nextInt(10000),
-      ));
-    }
-  }
-}
-
-final List<String> _names = <String>[
-  'Welli',
-  'Blonp',
-  'Folko',
-  'Furip',
-  'Folig',
-  'Picco',
-  'Frans',
-  'Warth',
-  'Linod',
-  'Simop',
-  'Merep',
-  'Riscu',
-  'Seves',
-  'Vaffe',
-  'Alfki'
-];
-
-final List<String> _designation = <String>[
-  'Project Lead',
-  'Developer',
-  'Manager',
-  'Designer',
-  'System Analyst',
-  'CEO'
-];
-
-class Employee {
-  Employee(this.id, this.name, this.designation, this.salary);
-
-  final int id;
-
-  final String name;
-
-  final String designation;
-
-  final int salary;
 }
 
 class EmployeeDataSource extends DataGridSource {
-  EmployeeDataSource({required List<Employee> employees}) {
-    _employeeData = employees
-        .map<DataGridRow>((e) => DataGridRow(cells: [
-              DataGridCell<int>(columnName: 'id', value: e.id),
-              DataGridCell<String>(columnName: 'name', value: e.name),
-              DataGridCell<String>(
-                  columnName: 'designation', value: e.designation),
-              DataGridCell<int>(columnName: 'salary', value: e.salary),
-            ]))
-        .toList();
-  }
-
-  List<DataGridRow> _employeeData = [];
+  final List<Employee> _employeeData = [];
 
   @override
-  List<DataGridRow> get rows => _employeeData;
+  List<DataGridRow> get rows => _employeeData.map<DataGridRow>((e) {
+        return DataGridRow(cells: [
+          DataGridCell<int>(columnName: 'id', value: e.id),
+          DataGridCell<String>(columnName: 'name', value: e.name),
+          DataGridCell<String>(columnName: 'designation', value: e.designation),
+          DataGridCell<int>(columnName: 'salary', value: e.salary),
+        ]);
+      }).toList();
 
-  void _addMoreRows(int count) {
-    final Random _random = Random();
-    int startIndex = _employeeData.isNotEmpty ? _employeeData.length : 0,
-        endIndex = startIndex + count;
-    for (int i = startIndex; i < endIndex; i++) {
-      _employeeData.add(DataGridRow(cells: [
-        DataGridCell<int>(columnName: 'id', value: 1000 + i),
-        DataGridCell<String>(
-            columnName: 'name',
-            value: _names[_random.nextInt(_names.length - 1)]),
-        DataGridCell<String>(
-            columnName: 'designation',
-            value: _designation[_random.nextInt(_designation.length - 1)]),
-        DataGridCell<int>(
-            columnName: 'salary', value: 10000 + _random.nextInt(10000)),
-      ]));
-    }
+  void addMoreRows(List<Employee> newData) {
+    _employeeData.addAll(newData);
+    // 데이터가 업데이트되었음을 알려야 합니다.
+    notifyListeners();
   }
 
-  @override
   Future<void> handleLoadMoreRows() async {
     await Future.delayed(Duration(seconds: 5));
-    _addMoreRows(10);
-    notifyListeners();
+    // 수정된 부분: 컨트롤러를 통해 데이터 추가
+    Get.find<LoadableTableController>().addMoreRows(10);
   }
 
   @override
   DataGridRowAdapter? buildRow(DataGridRow row) {
     return DataGridRowAdapter(
-        cells: row.getCells().map<Widget>((e) {
-      return Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Text(e.value.toString()),
-      );
-    }).toList());
+      cells: row.getCells().map<Widget>((e) {
+        return Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(e.value.toString()),
+        );
+      }).toList(),
+    );
   }
+}
+
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
+
+  final int id;
+  final String name;
+  final String designation;
+  final int salary;
 }
--- lib/component/table/loadable-table/loadable_table_controller.dart
+++ lib/component/table/loadable-table/loadable_table_controller.dart
@@ -1,4 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'dart:math';
 
-class LoadableTableController extends GetxController {}
+import 'package:get/get.dart';
+import 'package:namer_app/component/table/loadable-table/loadable_table.dart';
+
+class LoadableTableController extends GetxController {
+  final EmployeeDataSource employeeDataSource = EmployeeDataSource();
+
+  final List<String> names = <String>[
+    'Welli',
+    'Blonp',
+    'Folko',
+    'Furip',
+    'Folig',
+    'Picco',
+    'Frans',
+    'Warth',
+    'Linod',
+    'Simop',
+    'Merep',
+    'Riscu',
+    'Seves',
+    'Vaffe',
+    'Alfki'
+  ];
+
+  final List<String> designations = <String>[
+    'Project Lead',
+    'Developer',
+    'Manager',
+    'Designer',
+    'System Analyst',
+    'CEO'
+  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+    _populateEmployeeData(20);
+  }
+
+  void _populateEmployeeData(int count) {
+    final Random random = Random();
+    final List<Employee> employees = List.generate(count, (index) {
+      return Employee(
+        1000 + index,
+        names[random.nextInt(names.length)],
+        designations[random.nextInt(designations.length)],
+        10000 + random.nextInt(10000),
+      );
+    });
+    employeeDataSource.addMoreRows(employees);
+  }
+
+  void addMoreRows(int count) {
+    final Random random = Random();
+    final List<Employee> newEmployees = List.generate(count, (index) {
+      return Employee(
+        1000 + employeeDataSource.rows.length + index,
+        names[random.nextInt(names.length)],
+        designations[random.nextInt(designations.length)],
+        10000 + random.nextInt(10000),
+      );
+    });
+    employeeDataSource.addMoreRows(newEmployees);
+  }
+}
