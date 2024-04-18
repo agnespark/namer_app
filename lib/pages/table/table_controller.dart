54d3b73ba6818ba8c97805fd7d1c5c5f32724c9e
NAMYEJI <84209266+namYeJi9q@users.noreply.github.com>
2024-04-18 18:51:27 +0900

    [fix] typo

--- lib/pages/table/table_controller.dart
+++ lib/pages/table/table_controller.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class TableController extends GetxController {}
