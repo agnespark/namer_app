60b9e6f9cc153771a7d41bce2b47c6340e529e62
Agnes Kyuun Park <agnespark0413@gmail.com>
2024-04-18 17:09:00 +0900

    [refactor] basic_table 리팩토링

--- lib/component/table/basic-table/basic_table_controller.dart
+++ lib/component/table/basic-table/basic_table_controller.dart
@@ -1,1 +1,52 @@
+import 'package:get/get.dart';
+import 'package:namer_app/component/table/basic-table/basic_table.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
+class BasicTableController extends GetxController {
+  final EmployeeDataSource employeeDataSource = EmployeeDataSource();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _populateEmployeeData();
+  }
+
+  void _populateEmployeeData() {
+    final List<Employee> employees = [
+      Employee(10001, 'Jack', 'Manager', 120000),
+      Employee(10002, 'Perry', 'Project Lead', 80000),
+      Employee(10003, 'Lara', 'Developer', 45000),
+      Employee(10004, 'Ellis', 'Developer', 43000),
+      Employee(10005, 'Adams', 'Developer', 41000),
+      Employee(10006, 'Owens', 'QA Testing', 40000),
+      Employee(10007, 'Balnc', 'UX Designer', 39000),
+      Employee(10008, 'Steve', 'Support', 37000),
+      Employee(10009, 'Linda', 'Administrator', 36000),
+      Employee(10010, 'Michael', 'Sales Associate', 35000)
+    ];
+    employeeDataSource.addRows(_convertToDataGridRows(employees));
+  }
+
+  List<DataGridRow> _convertToDataGridRows(List<Employee> employees) {
+    return employees.map<DataGridRow>((e) {
+      return DataGridRow(cells: [
+        DataGridCell<int>(columnName: 'id', value: e.id),
+        DataGridCell<String>(columnName: 'name', value: e.name),
+        DataGridCell<String>(columnName: 'designation', value: e.designation),
+        DataGridCell<int>(columnName: 'salary', value: e.salary),
+      ]);
+    }).toList();
+  }
+
+  void addEmployeeData(List<Employee> newData) {
+    employeeDataSource.addRows(_convertToDataGridRows(newData));
+  }
+}
+
+class Employee {
+  Employee(this.id, this.name, this.designation, this.salary);
+  final int id;
+  final String name;
+  final String designation;
+  final int salary;
+}
